b0dd837b0f04cc642d0185fbb8bc9312f9747806
dragonchaser <1058116+dragonchaser@users.noreply.github.com>
2021-02-18 11:02:16 +0000

    Deploying to gh-pages from master @ 2225d1f9b05a9f7e657682699e3d8bffc82cbe73 🚀

--- tilesets/teleport.tsx
+++ tilesets/teleport.tsx
@@ -1,4 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="teleport" tilewidth="128" tileheight="64" tilecount="5" columns="1">
+<tileset version="1.4" tiledversion="1.4.3" name="teleport" tilewidth="128" tileheight="64" tilecount="5" columns="1" objectalignment="topleft">
  <image source="../assets/teleport_128.png" width="128" height="320"/>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="3" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="0" duration="5000"/>
+  </animation>
+ </tile>
 </tileset>
