bea7a0e735f66188a7a82596ae6ffce1fe499d25
dragonchaser <1058116+dragonchaser@users.noreply.github.com>
2021-02-22 07:48:28 +0000

    Deploying to gh-pages from master @ 043eabd9cae15ac3ee58059e9cb7df21a17ef8f8 🚀

--- tilesets/teleport.tsx
+++ tilesets/teleport.tsx
@@ -3,10 +3,38 @@
  <image source="../assets/teleport_128.png" width="128" height="320"/>
  <tile id="4">
   <animation>
-   <frame tileid="4" duration="250"/>
-   <frame tileid="3" duration="250"/>
-   <frame tileid="2" duration="250"/>
-   <frame tileid="1" duration="250"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
    <frame tileid="0" duration="5000"/>
   </animation>
  </tile>
