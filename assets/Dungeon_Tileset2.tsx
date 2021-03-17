4f970223323ab1eea8e7e66ffc1a9e76c68de9ba
Christian <crichter@suse.com>
2021-03-17 10:25:26 +0100

    add super secret map
    Signed-off-by: Christian Richter <christian@boltares.de>

---
+++ assets/Dungeon_Tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Dungeon_Tileset2" tilewidth="32" tileheight="32" tilecount="320" columns="16">
+ <image source="Dungeon_Tileset.png" trans="2f2f2e" width="512" height="640"/>
+</tileset>
